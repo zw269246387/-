43f0a1c74b903f8dea2203045fd78f02aaf00f1b
zhengwu <269246387@qq.com>
2018-08-28 16:17:23 +0800

    本地主干修改

--- 加密登录-钥匙串存储密码/ViewController.m
+++ 加密登录-钥匙串存储密码/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"本地主干代码");
+    
 }
 
 - (void)didReceiveMemoryWarning {
